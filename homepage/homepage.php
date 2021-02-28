869257240916be6114d644d3a677c4506cfc4559
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-02-28 13:20:56 -0500

    Added homepgage features and homepage.css + cleanup

---
+++ homepage/homepage.php
@@ -0,0 +1,33 @@
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>NHU homepage</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    
+    <body>
+        <div class="title">
+    	    <h1>Welcome to NHU website!</h1>
+        </div>
+
+        <div class="linkContainer">
+            <div class="logInContainer">
+                <h3>Log In</h3>
+            </div>
+
+            <div class="academicCalendarContainer">
+                <h3>Academic Calendar</h3>
+            </div>
+
+            <div class="catalogContainer">
+                <h3>College Catalog</h3>
+            </div>
+
+            <div class="masterScheduleContainer">
+                <h3>Master Schedule 2021</h3>
+            </div>
+        </div>
+    </body>
+</html>
