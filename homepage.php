d2779b7dcaceee9386012c2e01b119ec83d0d6bc
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-02-28 11:17:39 -0500

    set up

---
+++ homepage.php
@@ -0,0 +1,13 @@
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>NHU homepage</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+    	<h1>This is the Homepage</h1>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!doctype html>
+<html lang="en">
+<?php header("Location: http://localhost/nhu/homepage.php"); ?>
+    <head>
+        <title>NHU Index</title>
+        <meta charset="utf-8">
+
+    </head>
+    
+    <body>
+        <p>If you are not redirected, click 
+            <a href="http://localhost/nhu/homepage.php">this</a>
+        </p>
+
+
+    </body>
+</html>
