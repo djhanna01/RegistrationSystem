947b3c31e8ec8be99e2c22eeb1d2d50d17478937
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-03-01 12:52:03 -0500

    Added academic Calendar web page and css file

---
+++ academicCalendar/academicCalendar.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Academic Calendar 2021</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="academicCalendar.css">
+    </head>
+    <body>
+        <div>
+            <div>
+                <section>
+                    <header class="header">
+                        <h1>Academic Calendar - Spring 2021</h1>
+                    </header>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Event</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </section>
+            </div>
+        </div>
+    </body>
+</html>
