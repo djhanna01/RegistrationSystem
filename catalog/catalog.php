7f74078523f2067a9a92328f2dde17c26e72ac34
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-03-02 11:18:33 -0500

    added catalog webpage

---
+++ catalog/catalog.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+    <title>Catalog 2021</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="catalog.css">
+    </head>
+    <body>
+        <h1>Catalog Spring 2021</h1>
+        <h3>Undergraduate catalog<h3>
+
+        <h1>Catalog Fall 2022</h1>
+        <h3>Undergraduate catalog<h3>
+    </body>
+</html>
--- redirect.php
+++ redirect.php
@@ -10,7 +10,9 @@
 	else if($_POST["webpage"] == "academicCalendar"){
 		header("Location:  $baseURL/academicCalendar/academicCalendar.php"); 
 	}
-
+	else if($_POST["webpage"] == "catalog"){
+		header("Location: $baseURL/catalog/catalog.php");
+	}
 	else{
 		echo "didnt work";
 	}	
