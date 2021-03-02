1c8ad5e27baee65978d056ed09c5ad87511999b7
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-03-02 11:21:30 -0500

    added divs to components in catalog page

--- catalog/catalog.php
+++ catalog/catalog.php
@@ -1,15 +1,17 @@
 <!doctype html>
-<html>
+<html lang="en">
     <head>
     <title>Catalog 2021</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="catalog.css">
     </head>
     <body>
-        <h1>Catalog Spring 2021</h1>
-        <h3>Undergraduate catalog<h3>
+        <div>
+            <div><h1>Catalog Spring 2021</h1></div>
+            <div><h3>Undergraduate catalog<h3></div>
 
-        <h1>Catalog Fall 2022</h1>
-        <h3>Undergraduate catalog<h3>
+            <div><h1>Catalog Fall 2022</h1></div>
+            <div><h3>Undergraduate catalog<h3></div>
+        </div>
     </body>
 </html>
