d118e8d7bb931068be99ee7ed6f30f9064369378
pajonasvOW <vpajonas@oldwestbury.edu>
2021-03-11 16:38:10 -0500

    Login w/ all accounts
    (except researcher)

--- scripts/validateLogin.php
+++ scripts/validateLogin.php
@@ -10,17 +10,34 @@
     echo $username;
 
     $conn = connectToDB();
-    $sql = "SELECT * FROM LoginInfo WHERE email = " . $username . " && userType = 'Student' && pWord = " . $password;
+    $sql = "SELECT * FROM LoginInfo WHERE email = " . $username . " && pWord = " . $password;
 			
     $result = mysqli_query($conn, $sql);
     echo is_null($conn);
-	if ($result->num_rows > 0) {
+	if ($result->num_rows == 1) {
 	
 		setcookie("user", $username, time() + (86400 /24), "/"); // 86400 = 1 day
-		header("Location:  $baseURL/Student/studentHomepage.php"); 
+
+        $row = $result->fetch_row();
+        $userType = row[3];
+        if(strcmp($row[3], "Student") == 0){
+            header("Location:  $baseURL/Student/studentHomepage.php");
+        }
+        else if(strcmp($row[3], "Admin") == 0){
+            header("Location:  $baseURL/Admin/adminHomepage.php");
+        }
+        else if(strcmp($row[3], "Faculty") == 0){
+            header("Location:  $baseURL/Faculty/facultyHomepage.php");
+        }
+        else{
+          echo "Invalid user: ". $row[3];
+        }
   	}
-    else {
-        echo "it did not work...";
+    else if($result->num_rows < 0){
+        echo "Invalid credentials";
+    }
+    else{
+        echo "DUPLICATE ENTRY";
     }
 
 	mysqli_close($conn);
