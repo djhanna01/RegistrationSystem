e9d4b4ee442c534017f822fe7a7e22571d818d3f
pajonasvOW <vpajonas@oldwestbury.edu>
2021-04-04 20:46:35 -0400

    made Admin view courses into search for courses

--- Admin/viewListCourses.php
+++ Admin/viewListCourses.php
@@ -9,7 +9,7 @@
 ?>
 <html lang="en">
     <head>
-        <title>List Of Courses</title>
+        <title>Course Search</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="admin.css">
         <script type="text/javascript">
@@ -30,85 +30,56 @@
         </script>
     </head>
     <body>
-        <h1>List Of All Available Courses</h1>
-        <?php
+    <h1>Course Search</h1>
+	<form method="post" action="viewListCoursesResult.php">
+	<div>
+	<form  method="post" class="form" action="result.php">
+    <label for="department"><b>Department: </b></label>
+    	<select name="department" id="department">
+		<option value = '-1'>All</option>
+		<?php
 
         $conn = connectToDB();
-        $sql = "SELECT course.courseID, course.courseName, course.credits, department.departmentName
-                FROM course
-                LEFT JOIN department ON course.departmentID = department.departmentID";
+        $sql = "SELECT departmentName, departmentID
+                FROM Department";
         $result = mysqli_query($conn, $sql);
-        while ($row = $result->fetch_row()) {
-            
-            echo "
-            <details>
-            <summary>$row[0] - $row[1] - $row[2] credits - Dep of $row[3]</summary>
-            <table>
-                <thead>
-                    <tr>
-                        <th>CRN</th>
-                        <th>Course ID</th>
-                        <th>Professor</th>
-                        <th>Day</th>
-                        <th>Start Time</th>
-                        <th>End Time</th>
-                        <th>Start Date</th>
-                        <th>Seats Available</th>
-                        <th>Section Number</th>
-                        <th>Room</th>
-                    </tr>
-                </thead>
-                <tbody>
-                ";
-                $sqlB = "SELECT 
-                coursesection.CRN, 
-                Course.courseID, 
-                User.LName,
-                timeslotday.dayOfTheWeek, 
-                Period.startTime, 
-                Period.endTime, 
-                coursesection.startDate,
-                coursesection.seatsAvailable,
-                coursesection.sectionNumber,
-                Room.roomID
-                FROM coursesection
-                LEFT JOIN course ON coursesection.courseID = course.courseID
-                LEFT JOIN timeslotday ON coursesection.timeslotID = timeslotday.timeslotID
-                LEFT JOIN timeslotperiod ON coursesection.timeslotID = timeslotperiod.timeslotID
-                LEFT JOIN Period ON Period.periodNumber = timeslotperiod.periodNumber
-                LEFT JOIN Faculty ON Faculty.userID = coursesection.facultyID
-                LEFT JOIN User ON Faculty.userID = User.userID
-                LEFT JOIN Room ON Room.roomID = coursesection.roomID
-                WHERE course.courseID = '$row[0]'
-                GROUP BY CRN";
-                $subResult = mysqli_query($conn, $sqlB);
-                while ($subRow = $subResult->fetch_row()){
-                echo "
-                <tr>
-                ";
-                echo "<td>$subRow[0]</td>";
-                echo "<td>$subRow[1]</td>";
-                echo "<td>$subRow[2]</td>";
-                echo "<td>$subRow[3]</td>";
-                echo "<td>$subRow[4]</td>";
-                echo "<td>$subRow[5]</td>";
-                echo "<td>$subRow[6]</td>";
-                echo "<td>$subRow[7]</td>";
-                echo "<td>$subRow[8]</td>";
-                echo "<td>$subRow[9]</td>";
-                echo "
-                </tr>
-                ";
-                }
-            echo"
-            </tbody>
-            </table>
-            </details>
-            
-            ";
-        }
-        ?>
-        
-        
+		
+		while($row = $result->fetch_row()){
+			echo "<option value = '$row[1]'>$row[0]</option>";
+		}
+
+		?>
+   		</select>
+	</div>
+    <br></br>
+
+    <p><label for="courseType"><b>Course Type</b></label>
+            <select name="courseType" id="courseType">
+                <option value='any'>Any</option>
+                <option value='undergraduate'>Undergraduate</option>
+                <option value='graduate'>Graduate</option>
+            </select>
+    <br></br>
+
+    <div>
+    <label for="courseID"><b>Course ID: </b></label>
+    <input type="text" id="courseID" name="courseID">
+    </div>
+    <br></br>
+
+	<div>
+    <label for="courseName"><b>Course Name: </b></label>
+    <input type="text" id="courseName" name="courseName">
+    </div>
+    <br></br>
+
+
+	
+
+    <div>
+    	<input type="submit" value="Class Search">
+      	<input type ="reset" value="Reset">
+    </div>
+	</form>
     </body>
 </html>
