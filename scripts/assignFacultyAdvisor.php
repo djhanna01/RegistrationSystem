6206e13df4d0f8f4bb6c9f4af98034c1bf63ad53
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-04-06 13:12:02 -0400

    fixed duplicate issue in assign faculty as advisor

--- scripts/assignFacultyAdvisor.php
+++ scripts/assignFacultyAdvisor.php
@@ -21,7 +21,7 @@
             $conn = connectToDB();
 
             //check if faculty ID exits:
-            $sql = "SELECT * FROM Faculty where Faculty.userID = $facultyID";
+            $sql = "SELECT * FROM Faculty WHERE Faculty.userID = $facultyID";
             $result = mysqli_query($conn, $sql);
             if($result->num_rows==0){
                 echo "Faculty $facultyID does NOT exist.";
@@ -29,10 +29,18 @@
             }
 
             //check if student ID exists:
-            $sql = "SELECT * FROM Student where Student.userID = $studentID";
+            $sql = "SELECT * FROM Student WHERE Student.userID = $studentID";
             $result = mysqli_query($conn, $sql);
             if($result->num_rows==0){
                 echo "Student $studentID does NOT exist.";
+                die();
+            }
+
+            //check if faculty is already advising given student:
+            $sql = "SELECT * FROM Advises WHERE Advises.studentID = $studentID AND Advises.facultyID = $facultyID";
+            $result = mysqli_query($conn, $sql);
+            if($result->num_rows==1){
+                echo "Faculty $facultyID is already advising student $studentID.";
                 die();
             }
 
