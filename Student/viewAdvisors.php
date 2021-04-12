b92b2a8a151cee6ba6f0dabaf0c050e352c8a48d
Sebastian Huidobro <sebastianhuidobro1@gmail.com>
2021-04-11 20:58:08 -0400

    Attempting to fix the studentViewHolds.php conflict

--- Student/viewAdvisors.php
+++ Student/viewAdvisors.php
@@ -31,8 +31,49 @@
         </script>
     </head>
     <body>
-
         <h1>Advisors</h1>
+        <?php
+            $userID = $_COOKIE['userID'];
 
+            $conn = connectToDB();
+
+            //Lists the userID, name, email, and the student's ID who is being advised.
+            $sql = "SELECT
+            User.FName,
+            User.LName,
+            logininfo.email,
+            Advises.studentID
+            FROM User 
+            LEFT JOIN logininfo ON User.userID = logininfo.userID
+            LEFT JOIN advises ON User.userID = Advises.facultyID
+            WHERE studentID = $userID";
+
+            echo "
+        <table>
+        <thead>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Student's ID who is being advised.</th>
+        </tr>
+        </thead>
+        <tbody>
+        ";
+        $result = mysqli_query($conn, $sql);
+        while ($row = $result->fetch_row()) {
+            echo "<tr>";
+            echo "<td>$row[0]</td>";
+            echo "<td>$row[1]</td>";
+            echo "<td>$row[2]</td>";
+            echo "<td>$row[3]</td>";
+            echo "</tr>";
+          } 
+          echo "
+          </tbody>
+          </table>
+          ";
+        
+        ?>
     </body>
 </html>
