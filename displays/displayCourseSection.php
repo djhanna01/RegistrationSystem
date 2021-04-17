f67aee75ee7a0613ae73e3d030fe29e01b7874c6
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2021-04-17 17:19:01 -0400

    Faculty can now change the grade of a student in the view list of students after they click CRN from view classes taught

--- displays/displayCourseSection.php
+++ displays/displayCourseSection.php
@@ -8,86 +8,98 @@
       }
 ?>
 <html lang="en">
-<head>
+  <head>
 
-<meta charset="utf-8">
-<link rel="stylesheet" href="display.css">
-<script type="text/javascript">
-  function sendRedirectForm(value){
-                
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="display.css">
+  <script type="text/javascript">
+    function sendRedirectForm(value){
+                  
 
-    var submissionFrom = document.getElementById("redirectForm"); 
+      var submissionFrom = document.getElementById("redirectForm"); 
 
-    submissionFrom.innerHTML = "<input type = \"hidden\" name = \"courseID\" value = "+ value +" />" +
-    "<input type = \"hidden\" name = \"backPage\" value = \"homepage\" />";
+      submissionFrom.innerHTML = "<input type = \"hidden\" name = \"courseID\" value = "+ value +" />" +
+      "<input type = \"hidden\" name = \"backPage\" value = \"homepage\" />";
 
-    submissionFrom.submit();
-    }
-    function sendGoToCourseForm(value){
-                var submissionFrom = document.getElementById("gotoCourse"); 
-            
-                submissionFrom.innerHTML = "<input type = \"hidden\" name = \"courseID\" value = "+ value +" />" +
-                "<input type = \"hidden\" name = \"backPage\" value = \"homepage\" />";
-            
-                submissionFrom.submit();
-                }
-             
-</script>
+      submissionFrom.submit();
+      }
 
-</head>
-<body>
-<p><button type="submit" onclick="history.back()">Back</button>
+      function sendGoToCourseForm(value){
+        var submissionFrom = document.getElementById("gotoCourse"); 
+              
+        submissionFrom.innerHTML = "<input type = \"hidden\" name = \"courseID\" value = "+ value +" />" +
+                  "<input type = \"hidden\" name = \"backPage\" value = \"homepage\" />";
+              
+        submissionFrom.submit();
+      }       
+  </script>
 
+  </head>
+  <body>
+    <p><button type="submit" onclick="history.back()">Back</button>
 
-<?php 
-    $CRN = $_POST['CRN'];
-    
-    $conn = connectToDB();
-    $sql = "SELECT coursesection.*, user.FName, user.LName
-                FROM courseSection
-                LEFT JOIN user ON user.userID = coursesection.facultyID
-                WHERE CRN = $CRN";
-    $result = mysqli_query($conn, $sql);
-    //set variables here later
+    <form method="post" class="form" action="../scripts/facultyEditGrade.php" onsubmit="return confirm('Are you sure you want to submit the form?')">
+    <?php 
+        $CRN = $_POST['CRN'];
+        
+        $conn = connectToDB();
+        $sql = "SELECT coursesection.*, user.FName, user.LName
+                    FROM courseSection
+                    LEFT JOIN user ON user.userID = coursesection.facultyID
+                    WHERE CRN = $CRN";
+        $result = mysqli_query($conn, $sql);
+        //set variables here later
 
-    if(!$result){
-        echo "Something went wrong! $CRN";
-    }
+        if(!$result){
+            echo "Something went wrong! $CRN";
+        }
 
-    $sql = "SELECT enrollment.studentID, user.FName, user.LName, enrollment.enrollDate, enrollment.grade from enrollment
-                LEFT JOIN user ON user.userID = enrollment.studentID
-                WHERE enrollment.CRN = $CRN";
-    $result = mysqli_query($conn, $sql);
-
-
-  echo "
-<table>
-<thead>
-<tr>
-<th>Student ID</th>
-<th>Name</th>
-<th>Enroll Date</th>
-<th>Grade</th>
-</tr>
-</thead>
-<tbody>  
-";
+        $sql = "SELECT enrollment.studentID, user.FName, user.LName, enrollment.enrollDate, enrollment.grade from enrollment
+                    LEFT JOIN user ON user.userID = enrollment.studentID
+                    WHERE enrollment.CRN = $CRN";
+        $result = mysqli_query($conn, $sql);
 
 
-    while ($row = $result->fetch_row()) {
-      echo "<tr>";
-      echo "<td>$row[0]</td>";
-      echo "<td>$row[1] $row[2]</td>";
-      echo "<td>$row[3]</td>";
-      echo "<td>$row[4]</td>";
-    } 
-    echo "
-    </tbody>
-    </table>
+      echo "
+    <table>
+    <thead>
+    <tr>
+    <th>Student ID</th>
+    <th>Name</th>
+    <th>Enroll Date</th>
+    <th>Grade</th>
+    <th>Select To Change Grade</th>
+    </tr>
+    </thead>
+    <tbody>  
     ";
-  
 
 
-?>
-</body>
+        while ($row = $result->fetch_row()) {
+          echo "<tr>";
+          echo "<td>$row[0]</td>";
+          echo "<td>$row[1] $row[2]</td>";
+          echo "<td>$row[3]</td>";
+          echo "<td>$row[4]</td>";
+          echo "<td width='11%' class='pldefault'>
+                    <input type='radio' name='StudentID' value='$row[0]' id='StudentIDCheck' required>
+                </td>";
+        } 
+        echo "
+        </tbody>
+        </table>
+        ";
+        echo "<input type = 'hidden' name='CRN' value ='$CRN'>";
+    ?>
+      <h3>Please select a grade for the selected student above:</h3>
+      <select name='CourseGrade' id='courseGrade'>
+        <option value='A'>A</option>
+        <option value='B'>B</option>
+        <option value='C'>C</option>
+        <option value='D'>D</option>
+        <option value='F'>F</option>
+    </select>
+      <p><input type="submit" value="Submit"></p>
+    </form>
+  </body>
 </html>
