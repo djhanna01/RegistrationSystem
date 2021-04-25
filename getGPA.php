e69c74e335cd5141a63228e205004d9913351940
pajonasvOW <vpajonas@oldwestbury.edu>
2021-04-25 16:58:33 -0400

    facultyEditGrade now updates GPA

--- getGPA.php
+++ getGPA.php
@@ -8,28 +8,48 @@
 </head>
 <?php
 $conn = connectToDB();
-$sql = "SELECT userID, studentType, numOfCredits FROM student WHERE userID = 100";
+$sql = "SELECT userID, numOfCredits FROM student";
 
 $superResult = mysqli_query($conn, $sql);
 
 
 while($superRow = $superResult->fetch_row()){
     $studentID = $superRow[0];
-    $studentType = $superRow[1];
-    $numOfCredits = $superRow[2];
+    $numOfCredits = $superRow[1];
 
-    
-$sql = "SELECT COUNT(CRN) FROM enrollment
-        WHERE grade is null && studentID = $studentID && CRN NOT IN
-        (SELECT CRN FROM coursesection
-        WHERE semesterID = 1)";
+    $sql = "SELECT grade From Enrollment WHERE studentID = $studentID && grade IS NOT NULL";
+    $result = mysqli_query($conn, $sql);
+    $gradePoints = 0;
+    while($row = $result->fetch_row()){
+        switch($row[0]){
+            case "A":
+                $gradePoints += 16;
+                break;
+            case "B":
+                
+                $gradePoints += 12;
+                break;
+            case "C":
+                
+                $gradePoints += 8;
+                break;
+            case "D":
+                
+                $gradePoints += 4;
+                break;
+            case "F":
+                break;
+            default:
+                break;
+            
+        }
+    }
+   $GPA = $gradePoints/$numOfCredits;
 
-$result = mysqli_query($conn, $sql);
-$row = $result->fetch_row();
-echo $row[0];
-
-
-
+   $sql = "UPDATE undergradstudent SET GPA = $GPA WHERE userID = $studentID";
+   $result = mysqli_query($conn, $sql);
+   $sql = "UPDATE gradstudent SET GPA = $GPA WHERE userID = $studentID";
+   $result = mysqli_query($conn, $sql);
 
 }
 
