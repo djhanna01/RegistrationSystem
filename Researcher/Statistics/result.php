911c7b4cef07b5184b05b18e1207d1e8a36feecb
pajonasvOW <vpajonas@oldwestbury.edu>
2021-05-06 17:28:05 -0400

    averageSalary

--- Researcher/Statistics/result.php
+++ Researcher/Statistics/result.php
@@ -106,6 +106,89 @@
 
         }
 
+        if($type == "salary"){
+
+            if($facultyRank != "any"){
+                $rankWhere = " && faculty.facultyRank = '$facultyRank'";
+            }
+
+
+            if(($userType == "faculty" || $userType == "-1")){
+
+
+
+                if($facultyType == "Full Time" || $facultyType == "any"){
+                    $fulltimesql = "SELECT salary 
+                    FROM fulltimefaculty
+                    LEFT JOIN user ON user.userID = fulltimefaculty.userID
+                    LEFT JOIN faculty ON fulltimefaculty.userID = faculty.userID
+                    WHERE user.userID != -1 $genderWhere $rankWhere";
+                    
+                    $fulltimeFacultyResult = mysqli_query($conn, $fulltimesql);    
+                }
+                if($facultyType == "Part Time" || $facultyType == "any"){
+                    $parttimesql = "SELECT wageAmount
+                    FROM parttimefaculty
+                    LEFT JOIN user ON user.userID = parttimefaculty.userID
+                    LEFT JOIN faculty ON parttimefaculty.userID = faculty.userID
+                    WHERE user.userID != -1 $genderWhere $rankWhere";
+                    
+                    $parttimeFacultyResult = mysqli_query($conn, $parttimesql);
+                }
+                
+            }
+            if(($userType == "admin" || $userType == "-1") && $facultyRank == "any" && $facultyType == "any"){
+                $adminsql = "SELECT salary 
+                    FROM admin
+                    LEFT JOIN user ON user.userID = admin.userID
+                    WHERE user.userID != -1 $genderWhere";
+                    
+                    $adminResult = mysqli_query($conn, $adminsql);
+
+            }
+            if(($userType == "researcher" || $userType == "-1") && $facultyRank == "any" && $facultyType == "any"){
+                $researchersql = "SELECT salary 
+                    FROM researcher
+                    LEFT JOIN user ON user.userID = researcher.userID
+                    WHERE user.userID != -1 $genderWhere";
+                    
+                    $researcherResult = mysqli_query($conn, $researchersql);
+
+            }
+
+            if(!is_null($fulltimeFacultyResult)){
+                while($row = $fulltimeFacultyResult->fetch_row()){
+                    $total += $row[0];
+                    $count++;
+                }
+            }
+            if(!is_null($parttimeFacultyResult)){
+                while($row = $parttimeFacultyResult->fetch_row()){
+                    $total += $row[0];
+                    $count++;
+                }
+            }
+            if(!is_null($adminResult)){
+                while($row = $adminResult->fetch_row()){
+                    $total += $row[0];
+                    $count++;
+                }
+            }
+            if(!is_null($researcherResult)){
+                while($row = $researcherResult->fetch_row()){
+                    $total += $row[0];
+                    $count++;
+                }
+            }
+
+            $average = $total/$count;
+            if($count == 0){
+                $average = 0.00;
+            }
+            $average = round($average, 2);
+            echo "<br>Total entries: $count <br>Average Salary: $$average";
+        }
+
 
         if($type == "courseGrade"){
 
